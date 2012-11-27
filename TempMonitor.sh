c2b89546fcc86da778323217f4d390561afd624c
ed00m:th3d00m <ed00m@gnulinuxporqueno.cl>
2012-11-27 05:04:53 -0300

    version 2 de TempMonitor.sh

--- TempMonitor.sh
+++ TempMonitor.sh
@@ -1,32 +1,66 @@
-kernel=`uname -s -r -v` # -s, --kernel-name | -r, --kernel-release | -v, --kernel-version
-maquina=`uname -n -m -o` # -n, --nodename | -m, --machine | -p, --processor | -o, --operating-system | -i, --hardware-platform
-fecha=`date "+%Y%m%d"`
-fecha_log=`date "+%Y%m%d%H%M%S"`
-asunto="Monitor de temperatura "$maquina" - "$fecha_log
-log="../log/temperatura_$fecha_log.log"
- 
-echo "VERSION 0.6                           " >  $log
-echo "#-------------------------------------" >> $log
-echo "# MONITOR DE TEMPERATURA              " >> $log
-echo "#-------------------------------------" >> $log
-echo "# Maquina   : "$maquina                 >> $log
-echo "# Kernel    : "$kernel                  >> $log
-echo "# Fecha     : "$fecha                   >> $log
-echo "# Ejecucion : "$fecha_log               >> $log
+#!/bin/sh
+set -e
+#########################################
+# SCRIPT PARA MONITOREO DE TEMPERATURA
+# USA LM-SENSORS (SENSORS)
+# VERSION 2
+########################################
+
+# Opciones para kernel => -s, --kernel-name | -r, --kernel-release | -v, --kernel-version
+kernel=`uname -s -r -v`
+
+# Opciones para maquina => -n, --nodename | -m, --machine | -p, --processor | -o, --operating-system | -i, --hardware-platform
+maquina=`uname -n -m -o`
+
+fecha=`date "+%Y%m%d_%H%M%S"`
+log="/tmp/$$_$(date +"%Y%m%d_%H%M%S").log"
+asunto="Monitor de temperatura "${maquina}" - "${fecha}
+
+remitente=remitente@dominio.tld
+destinatarios=destinatario1@dominio.tld
+
+tempCritica=80
+tempWarning=70
+
+
+#########################################
+# EJECUCION
+#########################################
+
+echo "#-------------------------------------" >> ${log}
+echo "# MONITOR DE TEMPERATURA              " >> ${log}
+echo "#-------------------------------------" >> ${log}
+echo "# Maquina   : "${maquina}               >> ${log}
+echo "# Kernel    : "${kernel}                >> ${log}
+echo "# Fecha     : "{$fecha}                 >> ${log}
 echo ""
  
-sensors >> $log
+obtieneTemp=`sensors`
  
-temperatura=`grep 90 $log | cut -d " " -f8| cut -d "." -f1`
- 
-echo " * La temperatura es de "$temperatura" °C" >> $log
- 
-if [ $temperatura -gt 64 ]; then
-  if [ $temperatura -gt 80 ];then
-    cat $log|mailx -s "[CRITICO] $asunto" -r remitente -c remitentecopia destinatario
-    reboot
-  fi
-  if [[ $temperatura -gt 66 && $temperatura -lt 81 ]];then
-    cat $log|mailx -s "[WARNING] $asunto" -r remitente -c remitentecopia destinatario
+temperatura=`echo ${obtieneTemp}|grep +|cut -d "+" -f2|cut -d " " -f1`
+tempInt=`echo ${temperatura}|cut -d "." -f1`
+tempDec=`echo ${temperatura}|cut -d "." -f2`
+
+if [ ${tempDec} -gt 5 ]; then
+    temperatura=`expr ${tempInt} + 1`
+else
+    temperatura=${tempInt}
 fi
+
+echo " * La temperatura es de "$temperatura" °C" > ${log}
+ 
+if [ ${temperatura} -gt ${tempWarning} ]; then
+    if [ ${temperatura} -gt ${tempCritica} ];then
+        echo "* La temperatura es critica" >> ${log}
+        cat ${log}|mailx -s "[CRITICO] $asunto" -r ${remitente} ${destinatarios}
+        reboot
+    fi
+    if [[ ${temperatura} -gt ${tempWarning} && ${temperatura} -lt ${tempCritica} ]];then
+        echo "* Advertencia de temperatura " >> ${log}
+        cat $log|mailx -s "[WARNING] $asunto" -r ${remitente} ${destinatarios} 
+    fi
+else
+    echo "* La temperatura esta dentro de lo normal" >> ${log}
+fi
+
 exit 0
