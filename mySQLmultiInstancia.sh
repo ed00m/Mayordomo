3bd21dc5aa6310252b391552ad3754913d518239
Pablo <pabloze@gmail.com>
2013-05-22 07:25:50 -0400

    HF mySQLmultiInstancia.sh

--- mySQLmultiInstancia.sh
+++ mySQLmultiInstancia.sh
@@ -11,34 +11,36 @@ puertoNuevo=$(expr ${puerto} \* 2 + ${num})
 ###################################################
 
 #DATA
-mkdir /var/lib/${nombre}
-chown -R mysql.mysql /var/lib/${nombre}
+mkdir /var/lib/mysql-${nombre}
+chown -R mysql.mysql /var/lib/mysql-${nombre}
 
 #LOGS
-mkdir /var/log/${nombre}
-chown -R mysql.mysql /var/log/${nombre}
+mkdir /var/log/mysql-${nombre}
+chown -R mysql.mysql /var/log/mysql-${nombre}
 
 #CONFIGURACION
-if(cp -R /etc/mysql/ /etc/${nombre});then
-    if (mv /etc/${nombre}/my.cnf /etc/${nombre}/${nombre}.cnf);then
+if(cp -R /etc/mysql/ /etc/mysql-${nombre});then
+    if (mv /etc/${nombre}/my.cnf /etc/mysql-${nombre}/${nombre}.cnf);then
 
         #MODIFICAMOS CONFIGURACION PARA LA NUEVA INSTANCIA
-        sed -i "s/${puerto}/${puertoNuevo}/g" /etc/${nombre}/${nombre}.cnf
-        sed -i 's/mysqld.sock/mysqld2.sock/g' /etc/${nombre}/${nombre}.cnf
-        sed -i 's/mysqld.pid/mysqld2.pid/g' /etc/${nombre}/${nombre}.cnf
-        sed -i 's/var\/lib\/mysql/var\/lib\/mysql2/g' /etc/${nombre}/${nombre}.cnf
-        sed -i 's/var\/log\/mysql/var\/log\/mysql2/g' /etc/${nombre}/${nombre}.cnf
-        sed -i 's/\/etc\/mysql\//\/etc\/mysql2\//g' /etc/${nombre}/${nombre}.cnf
+        sed -i "s/${puerto}/${puertoNuevo}/g"                /etc/mysql-${nombre}/${nombre}.cnf
+        sed -i "s/mysqld.sock/${nombre}.sock/g"              /etc/mysql-${nombre}/${nombre}.cnf
+        sed -i "s/mysqld.pid/${nombre}.pid/g"                /etc/mysql-${nombre}/${nombre}.cnf
+        sed -i "s@/var/lib/mysql@/var/lib/mysql-${nombre}@g" /etc/mysql-${nombre}/${nombre}.cnf
+        sed -i "s@/var/log/mysql@/var/log/mysql-${nombre}@g" /etc/mysql-${nombre}/${nombre}.cnf
+        sed -i "s@/etc/mysql/@/etc/mysql-${nombre}/@g"       /etc/mysql-${nombre}/${nombre}.cnf
         
         #INICIALIZAMOS LA NUEVA INSTANCIA
-        mysql_install_db --user=mysql --datadir=/var/lib/${nombre}
+        mysql_install_db --user=mysql --datadir=/var/lib/mysql-${nombre}
 
         #INICIAMOS LA NUEVA INSTANCIA
-        mysqld_safe --defaults-file=/etc/${nombre}/${nombre}.cnf &
+        mysqld_safe --defaults-file=/etc/mysql-${nombre}/${nombre}.cnf &
 
         #CAMBIAMOS LA CLAVE DE ROOT
         nuevaClave=$(makepasswd -chars 24)
         /usr/bin/mysqladmin -S /var/run/mysqld/${nombre}.sock -u root password "${nuevaClave}"
-        echo "Nueva clave:".${nuevaClave}
+        echo "Nueva clave: ${nuevaClave}"
     fi
 fi
+
+exit 0
