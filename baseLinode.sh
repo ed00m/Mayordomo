200c24ee6aef7d9c49aba690bdaaaffdd18dae14
Pablo <pabloze@gmail.com>
2013-05-22 06:11:08 -0400

    Correo

--- baseLinode.sh
+++ baseLinode.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -x
 
-if (aptitude install git vim fail2ban rkhunter chkrootkit zsh psad sudo shorewall makepasswd openvpn);then
+if (aptitude install git vim fail2ban rkhunter chkrootkit zsh psad sudo shorewall makepasswd openvpn heirloom-mailx exim4);then
     echo "Software Instalado con exito"
 else
     echo "Problemas al instalar software"
