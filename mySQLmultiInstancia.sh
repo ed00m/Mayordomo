97f7b7ff1c9cd1b8c80c0f891148b9be59ac2855
Pablo <pabloze@gmail.com>
2013-05-22 11:17:26 -0400

    HF y ejemplod e conexion

--- mySQLmultiInstancia.sh
+++ mySQLmultiInstancia.sh
@@ -6,21 +6,31 @@ num=${2:?Numero Instancia}
 puerto=3306
 puertoNuevo=$(expr ${puerto} \* 2 + ${num})
 
+msg="ERROR se cancelara la instancia"
+
 ####################################################
 # DIRECTORIOS
 ###################################################
 
 #DATA
-mkdir /var/lib/mysql-${nombre}
-chown -R mysql.mysql /var/lib/mysql-${nombre}
+if (mkdir -p /var/lib/mysql-${nombre});then
+    chown -R mysql.mysql /var/lib/mysql-${nombre}
+else
+    echo ${msg}
+    exit 1
+fi
 
 #LOGS
-mkdir /var/log/mysql-${nombre}
-chown -R mysql.mysql /var/log/mysql-${nombre}
+if (mkdir -p /var/log/mysql-${nombre});then
+    chown -R mysql.mysql /var/log/mysql-${nombre}
+else
+    echo ${msg}
+    exit 1
+fi
 
 #CONFIGURACION
 if(cp -R /etc/mysql/ /etc/mysql-${nombre});then
-    if (mv /etc/${nombre}/my.cnf /etc/mysql-${nombre}/${nombre}.cnf);then
+    if (mv /etc/mysql/my.cnf /etc/mysql-${nombre}/${nombre}.cnf);then
 
         #MODIFICAMOS CONFIGURACION PARA LA NUEVA INSTANCIA
         sed -i "s/${puerto}/${puertoNuevo}/g"                /etc/mysql-${nombre}/${nombre}.cnf
@@ -40,7 +50,14 @@ if(cp -R /etc/mysql/ /etc/mysql-${nombre});then
         nuevaClave=$(makepasswd -chars 24)
         /usr/bin/mysqladmin -S /var/run/mysqld/${nombre}.sock -u root password "${nuevaClave}"
         echo "Nueva clave: ${nuevaClave}"
+        echo "Para probar la conexion prueba con"
+        echo "mysql -S /var/run/mysqld/${nombre}.sock -u root -p${nuevaClave}"
+        echo "Saludos"
+    else
+        echo ${msg}
     fi
+else
+    echo ${msg}
 fi
 
 exit 0
