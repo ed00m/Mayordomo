1be5181685779c0283af7948aced6d43bba686a9
Pablo <pabloze@gmail.com>
2013-06-10 20:13:52 -0400

    Normalizacion de repo

--- migradorUsuarios.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-for USUARIO in $(awk -F: '{ if ($3 >= '500' && $3 < '600' ) print $1 }' /etc/passwd);do
-    if [ -d /home/${USUARIO} ];then
-         mv /home/${USUARIO} /home/usuarios/${USUARIO}
-    else
-        echo "El home usuario ${USUARIO} ya fue migrado"
-    fi
-    sed -i -e "s@/home/${USUARIO}@/home/usuarios/${USUARIO}@g" /etc/passwd
-done
