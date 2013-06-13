7e6488f7f5b6e3909a49a1141aed0ab68b0a9a92
ed00m:th3d00m <pabloze@gmail.com>
2013-06-13 19:40:05 -0400

    V2 TempMonitor.sh

--- Sistema/TempMonitor.sh
+++ Sistema/TempMonitor.sh
@@ -1,5 +1,6 @@
-#!/bin/sh
+#!/bin/sh -x 
 set -e
+
 #########################################
 # SCRIPT PARA MONITOREO DE TEMPERATURA
 # USA LM-SENSORS (SENSORS)
@@ -14,32 +15,24 @@ maquina=`uname -n -m -o`
 
 fecha=`date "+%Y%m%d_%H%M%S"`
 log="/tmp/$$_${fecha}.log"
+
 asunto="Monitor de temperatura "${maquina}" - "${fecha}
 
 remitente=remitente@dominio.tld
-destinatarios=destinatario1@dominio.tld
+destinatarios=destinatarios@dominio.tld
 
 tempCritica=80
 tempWarning=70
 
+ACCION=${1:?Ingrese accion}
 
 #########################################
 # EJECUCION
 #########################################
-
-echo "#-------------------------------------" >> ${log}
-echo "# MONITOR DE TEMPERATURA              " >> ${log}
-echo "#-------------------------------------" >> ${log}
-echo "# Maquina   : "${maquina}               >> ${log}
-echo "# Kernel    : "${kernel}                >> ${log}
-echo "# Fecha     : "${$fecha}                 >> ${log}
-echo ""
  
-obtieneTemp=`sensors`
- 
-temperatura=`echo ${obtieneTemp}|grep +|cut -d "+" -f2|cut -d " " -f1`
-tempInt=`echo ${temperatura}|cut -d "." -f1`
-tempDec=`echo ${temperatura}|cut -d "." -f2`
+temperatura=`sensors|awk '/temp/ { print $2 }'`
+tempInt=`echo ${temperatura}|cut -d"." -f1|tr -dc 0-9`
+tempDec=`echo ${temperatura}|cut -d"." -f2|tr -dc 0-9`
 
 if [ ${tempDec} -gt 5 ]; then
     temperatura=`expr ${tempInt} + 1`
@@ -47,20 +40,58 @@ else
     temperatura=${tempInt}
 fi
 
-echo " * La temperatura es de "$temperatura" °C" >> ${log}
- 
-if [ ${temperatura} -gt ${tempWarning} ]; then
-    if [ ${temperatura} -gt ${tempCritica} ];then
-        echo "* La temperatura es critica" >> ${log}
-        cat ${log}|mailx -s "[CRITICO] $asunto" -r ${remitente} ${destinatarios}
-        reboot
-    fi
-    if [[ ${temperatura} -gt ${tempWarning} && ${temperatura} -lt ${tempCritica} ]];then
-        echo "* Advertencia de temperatura " >> ${log}
-        cat $log|mailx -s "[WARNING] $asunto" -r ${remitente} ${destinatarios} 
-    fi
-else
-    echo "* La temperatura esta dentro de lo normal" >> ${log}
-fi
+case ${ACCION} in
+
+    '--full')
+        
+        echo "#-------------------------------------" >> ${log}
+        echo "# MONITOR DE TEMPERATURA              " >> ${log}
+        echo "#-------------------------------------" >> ${log}
+        echo "# Maquina   : "${maquina}               >> ${log}
+        echo "# Kernel    : "${kernel}                >> ${log}
+        echo "# Fecha     : "${fecha}                >> ${log}
+        echo ""
+
+        echo "[x] La temperatura es de "$temperatura" °C" >> ${log}
+
+        if [ ${temperatura} -gt ${tempWarning} ]; then
+            
+            if [ ${temperatura} -gt ${tempCritica} ];then
+                echo "[x] La temperatura es critica" >> ${log}
+                cat ${log}|mailx -s "[CRITICO] $asunto" -r ${remitente} ${destinatarios}
+                reboot
+            fi
+
+            if [[ ${temperatura} -gt ${tempWarning} && ${temperatura} -lt ${tempCritica} ]];then
+                echo "[x] Advertencia de temperatura" >> ${log}
+                cat $log|mailx -s "[WARNING] $asunto" -r ${remitente} ${destinatarios} 
+            fi
+
+        else
+            echo "[x] La temperatura (${temperatura}) esta dentro de lo normal" >> ${log}
+        fi
+    ;;
+
+    '--info')
+
+        echo "#-------------------------------------"
+        echo "# MONITOR DE TEMPERATURA              "
+        echo "#-------------------------------------"
+        echo "# Maquina   : "${maquina}              
+        echo "# Kernel    : "${kernel}               
+        echo "# Fecha     : "${fecha}               
+        echo ""
+
+        echo " * La temperatura es de "$temperatura" °C"
+
+    ;;
+
+    *|'--help'|'--h'|'-h')
+        echo "[x] Acciones permitidas"
+        echo "--full: Captura informacion y envia reporte por correo"
+        echo "--info: Muestra informacion en pantalla"
+    ;;
+
+esac
 
 exit 0
